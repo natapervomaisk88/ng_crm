58989645a46fe396ebd682815dfaf452dc610992
Natalya Babenko <natapervomaisk88@gmail.com>
2024-03-03 20:58:20 +0200

    added new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ArticleComponent } from './components/article/article.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, ArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
